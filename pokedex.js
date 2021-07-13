8b27eb0b534a4dc3073ff3f7b3d3ff211f4652dc
bryson crandall <bryson.crandall@yahoo.com>
2021-07-13 14:11:30 -0700

    pokedex provides area pokemon are found in

--- pokedex.js
+++ pokedex.js
@@ -1,15 +1,27 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
 
-// read inFile and loop through each line
-
 const inFile = process.argv[2]
+
+
+const getLocation = (id) => {
+    return new Promise((resolve, reject) => {
+        resolve(
+            fetch(`https://pokeapi.co/api/v2/pokemon/${id}/encounters`)
+            .then(response => response.json())
+            .then((data) => { const loc = data[1].location_area.name;
+                    return loc })
+            .catch(() => { const loc = 'this pokemon has no known location';
+                    return loc}))
+        reject()
+    })
+}
 
 const pokedex = () => {
     return new Promise((resolve, reject) => {
         fs.readFile(inFile, 'utf-8', function (err, data) {
             if (err) {
-                reject('ERROR')
+                reject()
             }
             else {
                 resolve(data.split('\n'))
@@ -22,15 +34,21 @@ const pokedex = () => {
             fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
                 .then(response => response.json())
                 .then((data) => {
-                    let result = `${fmtPkm}: `
+                    let result = `${fmtPkm}: no.${data.id} type: `
                     for (let i = 0; i < data.types.length; i++) {
                         const ptype = data.types[i].type.name
                         result += ptype + (i < data.types.length - 1 ? ', ' : '')
                     }
-                    console.log(result)
+                    getLocation(data.id).then((location) => {
+                        result += ', found in: ' + location
+                        console.log(result)
+                    })
                 })
+                .catch(() => { return new Error(console.log(`${pokemon} is not a valid pokemon`)) })
         }
-    });
+    })
+        .catch(() => { return new Error(console.log('not enough arguements')) })
 }
+
 
 pokedex(inFile)
